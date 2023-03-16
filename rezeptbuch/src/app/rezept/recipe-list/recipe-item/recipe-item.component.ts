a1cf505e0f3404cb2ad677f0138fd12f99b40c0e
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2023-03-16 16:40:29 +0100

    updated recipe item and item list

--- rezeptbuch/src/app/rezept/recipe-list/recipe-item/recipe-item.component.ts
+++ rezeptbuch/src/app/rezept/recipe-list/recipe-item/recipe-item.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
 
