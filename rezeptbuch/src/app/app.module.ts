899b7d6eaeb25c8fcd4ef376a5eea2e48aa547da
gloria.tamakloe <gloria.tamakloe@bechtle.com>
2023-03-16 16:10:11 +0100

    added more components: recipe details+ recipe list

--- rezeptbuch/src/app/app.module.ts
+++ rezeptbuch/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { RezeptComponent } from './rezept/rezept.component';
 import { EinkaufslisteComponent } from './einkaufsliste/einkaufsliste.component';
+import { RecipeListComponent } from './rezept/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './rezept/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './rezept/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RezeptComponent,
-    EinkaufslisteComponent
+    EinkaufslisteComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ rezeptbuch/src/app/rezept/recipe-details/recipe-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details',
+  templateUrl: './recipe-details.component.html',
+})
+export class RecipeDetailsComponent {
+
+}
